19d6b4ba702bbd97329e1590619568851346b31a
Jacob Sachs <jacob.s.sachs@gmail.com>
2017-03-14 19:39:37 -0400

    add base test files for infeRS

---
+++ infer/tests/codetoanalyze/java/infer/StaticViewMemoryLeak.java
@@ -0,0 +1,12 @@
+package codetoanalyze.java.infer;
+
+import android.app.View;
+
+public class MyView extends View {
+
+  void setStaticView() {
+        view = findViewById(R.id.sv_button);
+    }
+
+}
+    
