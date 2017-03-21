bae85e2ec19eb823af7256cd52caa71494028e10
Alon Grinshpoon <alon.grinshpoon@yahoo.com>
2017-03-21 17:55:37 -0400

    [InfeRS] Reformating and Migrating LeakCanary to Option Activity

--- examples/android_infeRS/app/src/main/java/edu/columbia/cs/infers/MainApplication.java
+++ examples/android_infeRS/app/src/main/java/edu/columbia/cs/infers/MainApplication.java
@@ -6,13 +6,16 @@ import android.content.Context;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
 
-public class LeakCanaryApplication extends Application {
+public class MainApplication extends Application {
 
+    // LeakCanary Reference Watcher
     private RefWatcher refWatcher;
 
     @Override
     public void onCreate(){
         super.onCreate();
+
+        // LeakCanary Integration
         if (LeakCanary.isInAnalyzerProcess(this)) {
             // This process is dedicated to LeakCanary for heap analysis.
             // You should not init your app in this process.
@@ -21,8 +24,9 @@ public class LeakCanaryApplication extends Application {
         refWatcher = LeakCanary.install(this);
     }
 
+    // LeakCanary RefWatcher Getter
     public static RefWatcher getRefWatcher(Context context) {
-        LeakCanaryApplication application = (LeakCanaryApplication) context.getApplicationContext();
+        MainApplication application = (MainApplication) context.getApplicationContext();
         return application.refWatcher;
     }
 }
--- examples/android_infeRS/app/src/main/java/edu/columbia/cs/infers/SplashActivity.java
+++ examples/android_infeRS/app/src/main/java/edu/columbia/cs/infers/SplashActivity.java
@@ -11,8 +11,6 @@ import android.support.v4.app.ActivityCompat;
 import android.view.WindowManager;
 import android.widget.Toast;
 
-import com.squareup.leakcanary.RefWatcher;
-
 public class SplashActivity extends Activity {
 
     // Wait duration
@@ -23,41 +21,18 @@ public class SplashActivity extends Activity {
         super.onCreate(icicle);
 
         // Remove title bar
-        //this.requestWindowFeature(getWindow().FEATURE_NO_TITLE);
-        // Remove notification bar
-        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(getWindow().FEATURE_NO_TITLE);
 
+        // Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Set Layout
         setContentView(R.layout.activity_splash);
 
+        // Ask for Storage Permission (Used by LeakCanary)
         ActivityCompat.requestPermissions(SplashActivity.this,
                 new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                 1);
-
-        leakMemory();
-
-        /* New Handler to start the Menu-Activity 
-         * and close this splash screen after some wait time
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                // Create an Intent that will start the Menu Activity
-                Intent mainIntent = new Intent(SplashActivity.this,MenuActivity.class);
-                SplashActivity.this.startActivity(mainIntent);
-                // Close the Splash Activity
-                SplashActivity.this.finish();
-            }
-        }, SPLASH_DISPLAY_LENGTH);*/
-    }
-
-    private void leakMemory(){
-        new Thread() {
-            @Override
-            public void run(){
-                while (true){
-                    SystemClock.sleep(1000);
-                }
-            }
-        }.start();
     }
 
     @Override
@@ -65,24 +40,29 @@ public class SplashActivity extends Activity {
                                            String permissions[], int[] grantResults) {
         switch (requestCode) {
             case 1: {
-
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
+                    // Permission was granted
+                    // New Handler to start the Menu-Activity and close this splash screen after some wait time
+                    new Handler().postDelayed(new Runnable(){
+                        @Override
+                        public void run() {
+                            // Create an Intent that will start the Menu Activity
+                            Intent mainIntent = new Intent(SplashActivity.this,MenuActivity.class);
+                            SplashActivity.this.startActivity(mainIntent);
+                            // Close the Splash Activity
+                            SplashActivity.this.finish();
+                        }
+                    }, SPLASH_DISPLAY_LENGTH);
                 } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Toast.makeText(SplashActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                    // Permission denied
+                    Toast.makeText(SplashActivity.this, "Storage permission required.", Toast.LENGTH_SHORT).show();
+                    // Close the Splash Activity
+                    SplashActivity.this.finish();
                 }
                 return;
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
     }
 }
